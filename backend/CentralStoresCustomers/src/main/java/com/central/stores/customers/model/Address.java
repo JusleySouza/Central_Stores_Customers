5b89d4c736b1c88af338c34bda34e3e1129e3198
Jusley Souza <jusleysouza31@gmail.com>
2022-11-20 01:59:17 -0300

    Criação e implementação do modelo de Endereco

---
+++ backend/CentralStoresCustomers/src/main/java/com/central/stores/customers/model/Address.java
@@ -0,0 +1,43 @@
+package com.central.stores.customers.model;
+
+import java.util.Date;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class Address {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(updatable = false, nullable = false )
+	private UUID id;
+	@Column(nullable = false )
+	private String street;
+	@Column(nullable = false )
+	private Integer number;
+	@Column(nullable = false )
+	private String neighborhood;
+	@Column(nullable = false )
+	private String city;
+	@Column(nullable = false )
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
+	private Date created;
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
+	private Date changed;
+	
+	
+}
