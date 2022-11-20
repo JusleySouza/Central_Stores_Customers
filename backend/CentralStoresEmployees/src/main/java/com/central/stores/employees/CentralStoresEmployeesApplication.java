a0d2949364f82ddeb6d7a50b0706ce02da5837f9
ArturStefan <arthurstephan13@gmail.com>
2022-11-20 00:48:27 -0300

    Iniciando o projeto de funcionarios

---
+++ backend/CentralStoresEmployees/src/main/java/com/central/stores/employees/CentralStoresEmployeesApplication.java
@@ -0,0 +1,13 @@
+package com.central.stores.employees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralStoresEmployeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralStoresEmployeesApplication.class, args);
+	}
+
+}
