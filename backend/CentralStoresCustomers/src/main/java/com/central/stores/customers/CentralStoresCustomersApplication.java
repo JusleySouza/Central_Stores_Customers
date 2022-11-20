d282a6c5ac551131df60965c52041233cfeccaeb
Jusley Souza <jusleysouza31@gmail.com>
2022-11-20 00:41:54 -0300

    Iniciando o projeto de clientes

---
+++ backend/CentralStoresCustomers/src/main/java/com/central/stores/customers/CentralStoresCustomersApplication.java
@@ -0,0 +1,13 @@
+package com.central.stores.customers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralStoresCustomersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralStoresCustomersApplication.class, args);
+	}
+
+}
