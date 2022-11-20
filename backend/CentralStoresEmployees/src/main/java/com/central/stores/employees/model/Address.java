9ce0696a7a0c86fa3cf0eb2f7207134ecc2ba43c
ArturStefan <arthurstephan13@gmail.com>
2022-11-20 02:08:15 -0300

    Criação do model do endereço

---
+++ backend/CentralStoresEmployees/src/main/java/com/central/stores/employees/model/Address.java
@@ -0,0 +1,42 @@
+package com.central.stores.employees.model;
+
+import java.util.Date;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.central.stores.employees.constants.Conf;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class Address {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(updatable = false, nullable = false)
+	private UUID id;
+	@Column(nullable = false)
+	private String street;
+	@Column(nullable = false)
+	private Integer number;
+	@Column(nullable = false)
+	private String neighborhood;
+	@Column(nullable = false)
+	private String city;
+	@DateTimeFormat(pattern = Conf.dateFormat)
+	private Date created;
+	@DateTimeFormat(pattern = Conf.dateFormat)
+	private Date changed;
+}
