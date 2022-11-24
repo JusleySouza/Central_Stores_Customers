f4368a4aad1e0f214dfd2b992273ae8d854decfe
Jusley Souza <jusleysouza31@gmail.com>
2022-11-24 12:05:11 -0300

    Criação e implementação do modelo dto de Endereço

---
+++ backend/CentralStoresCustomers/src/main/java/com/central/stores/customers/model/dto/AddressDTO.java
@@ -0,0 +1,17 @@
+package com.central.stores.customers.model.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class AddressDTO {
+
+	private String street;
+	private Integer number;
+	private String neighborhood;
+	private String city;
+
+}
