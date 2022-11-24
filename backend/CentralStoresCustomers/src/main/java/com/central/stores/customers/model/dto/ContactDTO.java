1eeec58928aca6acd8c9dec45eff162b855c7082
Jusley Souza <jusleysouza31@gmail.com>
2022-11-24 12:05:39 -0300

    Criação e implementação do modelo dto de Contato

---
+++ backend/CentralStoresCustomers/src/main/java/com/central/stores/customers/model/dto/ContactDTO.java
@@ -0,0 +1,15 @@
+package com.central.stores.customers.model.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class ContactDTO {
+	
+	private String phone;
+	private String email;
+	
+}
