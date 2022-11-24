bf0c387eaf85249b2a6c67721cde1919666296a4
Jusley Souza <jusleysouza31@gmail.com>
2022-11-23 22:22:16 -0300

    Renomeando a classe

--- backend/CentralStoresCustomers/src/main/java/com/central/stores/customers/repository/CustomersRepository.java
+++ backend/CentralStoresCustomers/src/main/java/com/central/stores/customers/repository/CustomersRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.central.stores.customers.model.Customer;
 
-public interface CustomerRepository extends JpaRepository<Customer, UUID> {
+public interface CustomersRepository extends JpaRepository<Customer, UUID> {
 
 }
