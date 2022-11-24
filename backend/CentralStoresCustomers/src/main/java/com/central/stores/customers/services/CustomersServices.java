ca407fc7d8ce59997a151bf552910cb72183b6f3
Jusley Souza <jusleysouza31@gmail.com>
2022-11-24 12:14:13 -0300

    Criação e implementação da interface de serviços do Cliente

---
+++ backend/CentralStoresCustomers/src/main/java/com/central/stores/customers/services/CustomersServices.java
@@ -0,0 +1,26 @@
+package com.central.stores.customers.services;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.central.stores.customers.model.Customer;
+import com.central.stores.customers.model.dto.RequestCustomerDTO;
+import com.central.stores.customers.model.dto.ResponseCustomerDTO;
+
+@Service
+public interface CustomersServices {
+
+public ResponseEntity<List<Customer>> findAll();
+	
+	public ResponseEntity<Customer> findById(UUID id);
+	
+	public ResponseEntity<ResponseCustomerDTO> create(RequestCustomerDTO requestCustomerDTO);
+	
+	public ResponseEntity<Customer> update(RequestCustomerDTO requestCustomerDTO);
+	
+	public ResponseEntity<Customer> delete(UUID id);
+	
+}
