7930ea53f66c877e261443ac41b3a45946078494
ArturStefan <arthurstephan13@gmail.com>
2022-12-02 22:36:47 -0300

    Criação da rota para atualização dos dados do emprego

--- backend/CentralStoresEmployees/src/main/java/com/central/stores/employees/controller/EmployeesController.java
+++ backend/CentralStoresEmployees/src/main/java/com/central/stores/employees/controller/EmployeesController.java
@@ -1,8 +1,12 @@
 package com.central.stores.employees.controller;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,12 +18,18 @@ import com.central.stores.employees.services.EmployeesServices;
 @RestController
 @RequestMapping("/employees")
 public class EmployeesController {
-	
+
 	@Autowired
 	EmployeesServices services;
 
 	@PostMapping
-	public ResponseEntity<ResponseEmployeeDTO> create(@RequestBody RequestEmployeeDTO requestEmployeeDTO){
+	public ResponseEntity<ResponseEmployeeDTO> create(@RequestBody RequestEmployeeDTO requestEmployeeDTO) {
 		return services.create(requestEmployeeDTO);
+	}
+
+	@PutMapping("/{employeeId}")
+	public ResponseEntity<ResponseEmployeeDTO> update(@RequestBody RequestEmployeeDTO requestEmployeeDTO,
+			@PathVariable("employeeId") UUID employeeId) {
+		return services.update(requestEmployeeDTO, employeeId);
 	}
 }
