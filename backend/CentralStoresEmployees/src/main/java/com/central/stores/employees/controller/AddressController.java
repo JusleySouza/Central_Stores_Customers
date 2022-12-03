77b28181fcc19ea3f0cf3beca6add63a84a054fd
ArturStefan <arthurstephan13@gmail.com>
2022-12-02 23:45:17 -0300

    Criação da rota para manipulação dos dados de endereço do funcionário

--- backend/CentralStoresEmployees/src/main/java/com/central/stores/employees/controller/AddressController.java
+++ backend/CentralStoresEmployees/src/main/java/com/central/stores/employees/controller/AddressController.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.central.stores.employees.model.Address;
 import com.central.stores.employees.model.dto.AddressDTO;
 import com.central.stores.employees.services.AddressServices;
 
@@ -20,7 +22,14 @@ public class AddressController {
 	AddressServices services;
 
 	@PostMapping
-	public ResponseEntity<AddressDTO> create(@RequestBody AddressDTO requestAddressDTO, @PathVariable("employeeId") UUID employeeId ){
+	public ResponseEntity<Address> create(@RequestBody AddressDTO requestAddressDTO,
+			@PathVariable("employeeId") UUID employeeId) {
 		return services.create(requestAddressDTO, employeeId);
+	}
+
+	@PutMapping("/{addressId}")
+	public ResponseEntity<Address> update(@RequestBody AddressDTO requestAddressDTO,
+			@PathVariable("addressId") UUID addressId) {
+		return services.update(requestAddressDTO, addressId);
 	}
 }
