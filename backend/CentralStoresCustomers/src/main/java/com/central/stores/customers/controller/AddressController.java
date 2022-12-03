76508afb2716e0a74158add952e99588153517eb
Jusley Souza <jusleysouza31@gmail.com>
2022-12-02 21:11:10 -0300

    Criação do controller de Endereço

---
+++ backend/CentralStoresCustomers/src/main/java/com/central/stores/customers/controller/AddressController.java
@@ -0,0 +1,10 @@
+package com.central.stores.customers.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customers/{customerId}/address")
+public class AddressController {
+
+}
