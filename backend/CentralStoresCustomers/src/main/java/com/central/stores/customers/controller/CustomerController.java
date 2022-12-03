60f36780a921976bebe28e5b576743b75e5a7e50
Jusley Souza <jusleysouza31@gmail.com>
2022-12-02 22:38:59 -0300

    Implementação da rota de update

--- backend/CentralStoresCustomers/src/main/java/com/central/stores/customers/controller/CustomerController.java
+++ backend/CentralStoresCustomers/src/main/java/com/central/stores/customers/controller/CustomerController.java
@@ -1,8 +1,12 @@
 package com.central.stores.customers.controller;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,13 +18,18 @@ import com.central.stores.customers.services.CustomersServices;
 @RestController
 @RequestMapping("/customers")
 public class CustomerController {
-	
+
 	@Autowired
 	CustomersServices services;
 
 	@PostMapping
-	public ResponseEntity<ResponseCustomerDTO> create(@RequestBody RequestCustomerDTO requestCustomerDTO){
+	public ResponseEntity<ResponseCustomerDTO> create(@RequestBody RequestCustomerDTO requestCustomerDTO) {
 		return services.create(requestCustomerDTO);
 	}
-	
+
+	@PutMapping("/{customerId}")
+	public ResponseEntity<ResponseCustomerDTO> update(@RequestBody RequestCustomerDTO requestCustomerDTO,
+			@PathVariable("customerId") UUID customerId) {
+		return services.update(requestCustomerDTO, customerId);
+	}
 }
