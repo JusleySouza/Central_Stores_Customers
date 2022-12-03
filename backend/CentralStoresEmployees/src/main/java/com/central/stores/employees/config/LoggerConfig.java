ab420fd775f3c843bcc5888532234a169b1bd984
ArturStefan <arthurstephan13@gmail.com>
2022-12-02 23:07:22 -0300

    Criação da classe de configuração dos logs

---
+++ backend/CentralStoresEmployees/src/main/java/com/central/stores/employees/config/LoggerConfig.java
@@ -0,0 +1,13 @@
+package com.central.stores.employees.config;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.central.stores.employees.services.implement.AddressServicesImplent;
+import com.central.stores.employees.services.implement.EmployeesServicesImplement;
+
+public final class LoggerConfig {
+	public static final Logger LOGGER_EMPLOYEE = LoggerFactory.getLogger(EmployeesServicesImplement.class);
+	public static final Logger LOGGER_ADDRESS = LoggerFactory.getLogger(AddressServicesImplent.class);
+}
