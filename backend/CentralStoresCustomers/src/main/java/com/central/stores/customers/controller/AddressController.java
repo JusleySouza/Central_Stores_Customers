cc20293be0a72163585308158d45d04bdd6bbb52
Jusley Souza <jusleysouza31@gmail.com>
2022-12-02 21:57:56 -0300

    Criação e implementação da rota de criação de endereço.

--- backend/CentralStoresCustomers/src/main/java/com/central/stores/customers/controller/AddressController.java
+++ backend/CentralStoresCustomers/src/main/java/com/central/stores/customers/controller/AddressController.java
@@ -1,10 +1,28 @@
 package com.central.stores.customers.controller;
 
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.central.stores.customers.model.dto.AddressDTO;
+import com.central.stores.customers.services.AddressServices;
 
 @RestController
 @RequestMapping("/customers/{customerId}/address")
 public class AddressController {
+	
+	@Autowired
+	AddressServices services;
+	
+	@PostMapping
+	public ResponseEntity<AddressDTO> create(@RequestBody AddressDTO requestAddressDTO, @PathVariable("customerId") UUID customerId) {
+		return services.create(requestAddressDTO, customerId);
+	}
 
 }
