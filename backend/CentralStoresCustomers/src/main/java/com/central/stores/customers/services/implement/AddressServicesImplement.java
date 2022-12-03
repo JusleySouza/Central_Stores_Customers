8e53ac7ca7f81a4a39e1c19da9c51a21ded42563
Jusley Souza <jusleysouza31@gmail.com>
2022-12-02 21:56:19 -0300

    Implementação do serviço de criação de endereço.

--- backend/CentralStoresCustomers/src/main/java/com/central/stores/customers/services/implement/AddressServicesImplement.java
+++ backend/CentralStoresCustomers/src/main/java/com/central/stores/customers/services/implement/AddressServicesImplement.java
@@ -3,15 +3,29 @@ package com.central.stores.customers.services.implement;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import com.central.stores.customers.model.Address;
+import com.central.stores.customers.model.Customer;
 import com.central.stores.customers.model.dto.AddressDTO;
+import com.central.stores.customers.repository.AddressRepository;
+import com.central.stores.customers.repository.CustomersRepository;
 import com.central.stores.customers.services.AddressServices;
 
 @Component
 public class AddressServicesImplement implements AddressServices {
+	
+	@Autowired
+	CustomersRepository customerRepository;
+	
+	@Autowired
+	AddressRepository addressRepository;
+	
+	Address address;
+	Customer customer;
 
 	@Override
 	public ResponseEntity<List<Address>> findAll() {
@@ -27,8 +41,14 @@ public class AddressServicesImplement implements AddressServices {
 
 	@Override
 	public ResponseEntity<AddressDTO> create(AddressDTO requestAddressDTO, UUID customerId) {
-		// TODO Auto-generated method stub
-		return null;
+		address = new Address();
+		customer = new Customer();
+		address.transformRequestAddressDTOToModel(requestAddressDTO);
+		addressRepository.save(address);
+		customer = customerRepository.findById(customerId).get();
+		customer.setAddress(address);
+		customerRepository.save(customer);
+		return new ResponseEntity<AddressDTO>(HttpStatus.CREATED);
 	}
 
 	@Override
