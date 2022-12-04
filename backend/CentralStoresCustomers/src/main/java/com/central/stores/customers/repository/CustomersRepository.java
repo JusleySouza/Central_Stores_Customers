dfe941ebd0dc920b59dad37438cf4d777e4daef7
Jusley Souza <jusleysouza31@gmail.com>
2022-12-04 00:10:49 -0300

    Criação da consulta customizada para trazer os clientes ativos

--- backend/CentralStoresCustomers/src/main/java/com/central/stores/customers/repository/CustomersRepository.java
+++ backend/CentralStoresCustomers/src/main/java/com/central/stores/customers/repository/CustomersRepository.java
@@ -1,5 +1,6 @@
 package com.central.stores.customers.repository;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.central.stores.customers.model.Customer;
 
 public interface CustomersRepository extends JpaRepository<Customer, UUID> {
-
+	List<Customer> findAllByActiveTrue();
 }
