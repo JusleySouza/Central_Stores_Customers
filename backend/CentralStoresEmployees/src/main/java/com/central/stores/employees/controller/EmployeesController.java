37051c813a1fa9fec1420d5bc6b129899c1af4fc
ArturStefan <arthurstephan13@gmail.com>
2022-12-04 00:11:04 -0300

    Criação de rota para listagem de clientes

--- backend/CentralStoresEmployees/src/main/java/com/central/stores/employees/controller/EmployeesController.java
+++ backend/CentralStoresEmployees/src/main/java/com/central/stores/employees/controller/EmployeesController.java
@@ -1,10 +1,12 @@
 package com.central.stores.employees.controller;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.central.stores.employees.model.Employee;
 import com.central.stores.employees.model.dto.RequestEmployeeDTO;
 import com.central.stores.employees.model.dto.ResponseEmployeeDTO;
 import com.central.stores.employees.services.EmployeesServices;
@@ -37,5 +40,10 @@ public class EmployeesController {
 	@DeleteMapping("/{employeeId}")
 	public ResponseEntity<ResponseEmployeeDTO> delete(@PathVariable("employeeId") UUID employeeId){
 		return services.delete(employeeId);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Employee>> listEmployees(){
+		return services.findAll();
 	}
 }
