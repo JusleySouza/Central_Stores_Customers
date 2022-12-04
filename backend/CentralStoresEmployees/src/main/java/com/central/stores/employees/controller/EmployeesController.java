fa25dbb077d9900b664a69c743b1c779c3ba419b
ArturStefan <arthurstephan13@gmail.com>
2022-12-03 23:26:20 -0300

    Criação da rota para soft delete

--- backend/CentralStoresEmployees/src/main/java/com/central/stores/employees/controller/EmployeesController.java
+++ backend/CentralStoresEmployees/src/main/java/com/central/stores/employees/controller/EmployeesController.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -31,5 +32,10 @@ public class EmployeesController {
 	public ResponseEntity<ResponseEmployeeDTO> update(@RequestBody RequestEmployeeDTO requestEmployeeDTO,
 			@PathVariable("employeeId") UUID employeeId) {
 		return services.update(requestEmployeeDTO, employeeId);
+	}
+	
+	@DeleteMapping("/{employeeId}")
+	public ResponseEntity<ResponseEmployeeDTO> delete(@PathVariable("employeeId") UUID employeeId){
+		return services.delete(employeeId);
 	}
 }
