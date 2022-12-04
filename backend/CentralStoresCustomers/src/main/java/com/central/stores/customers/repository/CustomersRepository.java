01978f5fd58678416d7f039a7ddba36fe7e995e2
Jusley Souza <jusleysouza31@gmail.com>
2022-12-04 00:43:15 -0300

    Criação da consulta do cliente por cpf

--- backend/CentralStoresCustomers/src/main/java/com/central/stores/customers/repository/CustomersRepository.java
+++ backend/CentralStoresCustomers/src/main/java/com/central/stores/customers/repository/CustomersRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.ResponseEntity;
 
 import com.central.stores.customers.model.Customer;
 
 public interface CustomersRepository extends JpaRepository<Customer, UUID> {
 	List<Customer> findAllByActiveTrue();
+	Customer findByCpf(String cpf);
 }
