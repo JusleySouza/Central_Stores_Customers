227a66ebb97ab83a01cda8275a3461dad1ea6a47
ArturStefan <arthurstephan13@gmail.com>
2022-12-04 01:33:49 -0300

    Remoção de metodos não utilizados

--- backend/CentralStoresEmployees/src/main/java/com/central/stores/employees/services/AddressServices.java
+++ backend/CentralStoresEmployees/src/main/java/com/central/stores/employees/services/AddressServices.java
@@ -1,6 +1,5 @@
 package com.central.stores.employees.services;
 
-import java.util.List;
 import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
@@ -11,13 +10,8 @@ import com.central.stores.employees.model.dto.AddressDTO;
 
 @Service
 public interface AddressServices {
-	public ResponseEntity<List<AddressDTO>> findAll();
-	
-	public ResponseEntity<AddressDTO> findById(UUID addressId);
 	
 	public ResponseEntity<Address> create(AddressDTO requestAddressDTO, UUID employeeId);
 	
 	public ResponseEntity<Address> update(AddressDTO requestAddressDTO, UUID addressId);
-	
-	public ResponseEntity<AddressDTO> delete(UUID addressId);
 }
