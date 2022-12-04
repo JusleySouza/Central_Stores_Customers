e087b52873199c452896e1422d8aaf5da118f911
Jusley Souza <jusleysouza31@gmail.com>
2022-12-04 01:25:45 -0300

    Criação da query de consulta de cliente por bairro

--- backend/CentralStoresCustomers/src/main/java/com/central/stores/customers/repository/CustomersRepository.java
+++ backend/CentralStoresCustomers/src/main/java/com/central/stores/customers/repository/CustomersRepository.java
@@ -4,11 +4,11 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.http.ResponseEntity;
 
 import com.central.stores.customers.model.Customer;
 
 public interface CustomersRepository extends JpaRepository<Customer, UUID> {
 	List<Customer> findAllByActiveTrue();
 	Customer findByCpf(String cpf);
+	List<Customer> findAllByActiveTrueAndAddressNeighborhood(String neighborhood);
 }
