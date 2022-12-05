1f1d81ca55026edc03fd9e447744c3fd998b93cc
Jusley Souza <jusleysouza31@gmail.com>
2022-12-04 23:31:55 -0300

    Remoção do método de transformeModelToDTO

--- backend/CentralStoresCustomers/src/main/java/com/central/stores/customers/model/dto/ResponseCustomerDTO.java
+++ backend/CentralStoresCustomers/src/main/java/com/central/stores/customers/model/dto/ResponseCustomerDTO.java
@@ -2,8 +2,6 @@ package com.central.stores.customers.model.dto;
 
 import java.util.UUID;
 
-import com.central.stores.customers.model.Customer;
-
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -15,10 +13,5 @@ public class ResponseCustomerDTO {
 
 	private UUID id;
 	private String name;
-	
-	public void transformModelToResponseCustomerDTO(Customer customer) {
-		this.id = customer.getId();
-		this.name = customer.getName();
-	}
 
 }
