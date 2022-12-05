bc7b3227a0c44880013412d01ea557cba511099c
ArturStefan <arthurstephan13@gmail.com>
2022-12-04 23:26:21 -0300

    Criação do mapper da classe de endereço

---
+++ backend/CentralStoresEmployees/src/main/java/com/central/stores/employees/mapper/AddressMapper.java
@@ -0,0 +1,28 @@
+package com.central.stores.employees.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import com.central.stores.employees.model.Address;
+import com.central.stores.employees.model.dto.AddressDTO;
+
+@Mapper(componentModel = "spring")
+public interface AddressMapper {
+	@Mappings({
+		@Mapping(target= "id", ignore= true),
+		@Mapping(target= "changed", ignore= true),
+		@Mapping(target= "created", expression = "java(java.time.LocalDate.now())"),
+	})
+	
+	Address toModel(AddressDTO addressDTO);
+	
+	@Mappings({
+		@Mapping(target= "id", ignore= true),
+		@Mapping(target= "created", ignore= true),
+		@Mapping(target= "changed", expression = "java(java.time.LocalDate.now())"),
+	})
+	
+	Address updateModel(AddressDTO requestAddressDTO);
+	
+}
