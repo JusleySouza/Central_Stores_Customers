096a03bafb7000fe992f12eaf80810d99eaa46a3
Jusley Souza <jusleysouza31@gmail.com>
2022-12-06 00:55:31 -0300

    Criação e implementação da classe de testes dos serviços

---
+++ backend/CentralStoresCustomers/src/test/java/com/central/stores/customers/services/implement/CustomerServicesImplementTest.java
@@ -0,0 +1,73 @@
+package com.central.stores.customers.services.implement;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.ResponseEntity;
+
+import com.central.stores.customers.crypto.Cryptography;
+import com.central.stores.customers.model.Customer;
+import com.central.stores.customers.model.dto.RequestCustomerDTO;
+import com.central.stores.customers.model.dto.ResponseCustomerDTO;
+import com.central.stores.customers.repository.CustomersRepository;
+import com.central.stores.customers.services.implement.CustomersServicesImplement;
+
+
+class CustomerServicesImplementTest {
+
+	@InjectMocks
+	private CustomersServicesImplement services;
+	
+	@Mock
+	private CustomersRepository repository;
+	
+	private Customer customer;
+	private RequestCustomerDTO requestCustomerDTO;
+	private ResponseCustomerDTO responseCustomerDTO;
+	private ResponseEntity<Customer> responseEntity;
+	
+	@BeforeEach
+	void setUp() {
+		MockitoAnnotations.openMocks(this);
+		customer = new Customer();
+		requestCustomerDTO = new RequestCustomerDTO();
+		responseCustomerDTO = new ResponseCustomerDTO();
+		
+		customer.setActive(true);
+		customer.setChanged(LocalDate.now());
+		customer.setCpf("12365478965");
+		customer.setCreated(LocalDate.now());
+		customer.setEmail("caio@castro.com");
+		customer.setGender("masculino");
+		customer.setName("Caio Castro");
+		customer.setPhone("1111111111");
+		customer.setRg("325698741");
+		
+		requestCustomerDTO.setCpf("12365478965");
+		requestCustomerDTO.setEmail("caio@castro.com");
+		requestCustomerDTO.setGender("masculino");
+		requestCustomerDTO.setName("Caio Castro");
+		requestCustomerDTO.setPhone("1111111111");
+		requestCustomerDTO.setRg("325698741");
+		
+		responseCustomerDTO.setName("Caio Castro");
+		
+	}
+	
+	@Test
+	public void findByCpf() throws Exception {
+		customer = Cryptography.encode(customer);
+		when(repository.findByCpf(anyString())).thenReturn(customer);
+		Customer cust = services.findByCpf("12365478965").getBody();
+		assertEquals(cust, customer);
+	}
+
+}
