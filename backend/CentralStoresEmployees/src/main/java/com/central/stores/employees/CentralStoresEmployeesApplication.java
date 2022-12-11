784b0ff1e688906fd50fb43dc7169df769d38277
ArturStefan <arthurstephan13@gmail.com>
2022-12-11 16:39:28 -0300

    Adição da anotation generated

--- backend/CentralStoresEmployees/src/main/java/com/central/stores/employees/CentralStoresEmployeesApplication.java
+++ backend/CentralStoresEmployees/src/main/java/com/central/stores/employees/CentralStoresEmployeesApplication.java
@@ -3,11 +3,13 @@ package com.central.stores.employees;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.Generated;
+
+@Generated
 @SpringBootApplication
 public class CentralStoresEmployeesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CentralStoresEmployeesApplication.class, args);
 	}
-
 }
