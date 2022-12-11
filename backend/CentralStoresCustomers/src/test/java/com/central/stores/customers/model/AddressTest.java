942f86790ef250b2f0893021718e0e0f26bc5c85
Jusley Souza <jusleysouza31@gmail.com>
2022-12-11 19:51:02 -0300

    Adicionando o id

--- backend/CentralStoresCustomers/src/test/java/com/central/stores/customers/model/AddressTest.java
+++ backend/CentralStoresCustomers/src/test/java/com/central/stores/customers/model/AddressTest.java
@@ -19,12 +19,13 @@ class AddressTest {
 	void setUp() {
 		id = UUID.randomUUID();
 		expectedAddress = ClassBuilder.addressBuilder();
+		expectedAddress.setId(id);
 	}
 
 	@Test
 	void build() {
 		Address address = Address.builder().city("Bauru").neighborhood("Pedro Santos Drumond").number(56).street("Rua das Amelias")
-				.changed(LocalDate.now()).created(LocalDate.now()).build();
+				.changed(LocalDate.now()).created(LocalDate.now()).id(id).build();
 		assertEquals(expectedAddress.toString(), address.toString());
 	}
 	
@@ -38,6 +39,7 @@ class AddressTest {
 		address.setStreet("Rua das Amelias");
 		address.setChanged(LocalDate.now());
 		address.setCreated(LocalDate.now());
+		address.setId(id);
 		assertEquals(expectedAddress.toString(), address.toString());
 	}
 
