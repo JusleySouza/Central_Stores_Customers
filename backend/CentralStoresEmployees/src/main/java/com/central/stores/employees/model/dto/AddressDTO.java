a68c385938314061f456258e80273eb77786e6b0
ArturStefan <arthurstephan13@gmail.com>
2022-12-11 16:40:55 -0300

    Adição de anotations

--- backend/CentralStoresEmployees/src/main/java/com/central/stores/employees/model/dto/AddressDTO.java
+++ backend/CentralStoresEmployees/src/main/java/com/central/stores/employees/model/dto/AddressDTO.java
@@ -1,12 +1,18 @@
 package com.central.stores.employees.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @Setter
+@Builder
 @ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class AddressDTO {
 	private String street;
 	private Integer number;
