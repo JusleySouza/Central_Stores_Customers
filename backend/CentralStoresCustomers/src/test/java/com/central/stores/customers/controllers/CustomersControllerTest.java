14000ab95a95325d9f5b1b792bbacfc5b597fe58
Jusley Souza <jusleysouza31@gmail.com>
2022-12-11 14:08:35 -0300

    Criação da Classe de teste do customer controller

---
+++ backend/CentralStoresCustomers/src/test/java/com/central/stores/customers/controllers/CustomersControllerTest.java
@@ -0,0 +1,19 @@
+package com.central.stores.customers.controllers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+
+class CustomersControllerTest {
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
