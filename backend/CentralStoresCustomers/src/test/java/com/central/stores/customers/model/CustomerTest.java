ba2e0accaef70e2d28de2933b979a5c07b207041
Jusley Souza <jusleysouza31@gmail.com>
2022-12-11 19:06:49 -0300

    Criação e implementação do teste de setter

--- backend/CentralStoresCustomers/src/test/java/com/central/stores/customers/model/CustomerTest.java
+++ backend/CentralStoresCustomers/src/test/java/com/central/stores/customers/model/CustomerTest.java
@@ -16,21 +16,41 @@ class CustomerTest {
 	private Customer expectedCustomer;
 	
 	private UUID id;
+	
+	private Address address;
 
 	@BeforeEach
 	void setUp() throws Exception {
 		id = UUID.randomUUID();
+		address = ClassBuilder.addressBuilder();
 		expectedCustomer = ClassBuilder.customerBuider();
 		expectedCustomer.setId(id);
+		expectedCustomer.setAddress(address);
 	}
 
 	@Test
 	void builder() {
 		Customer customer = Customer.builder().active(true).changed(LocalDate.now()).cpf("12365478965").created(LocalDate.now())
-				.email("caio@castro.com").gender("masculino").name("Caio Castro").phone("1111111111").rg("325698741").id(id)
+				.email("caio@castro.com").gender("masculino").name("Caio Castro").phone("1111111111").rg("325698741").id(id).address(address)
 				.build();
 		assertEquals(expectedCustomer.toString(), customer.toString());
-		
+	}
+	
+	@Test
+	void setter() {
+		Customer customer = new Customer();
+		customer.setActive(true);
+		customer.setChanged(LocalDate.now());
+		customer.setCpf("12365478965");
+		customer.setCreated(LocalDate.now());
+		customer.setEmail("caio@castro.com");
+		customer.setGender("masculino");
+		customer.setName("Caio Castro");
+		customer.setPhone("1111111111");
+		customer.setRg("325698741");
+		customer.setId(id);
+		customer.setAddress(address);
+		assertEquals(expectedCustomer.toString(), customer.toString());
 	}
 
 }
