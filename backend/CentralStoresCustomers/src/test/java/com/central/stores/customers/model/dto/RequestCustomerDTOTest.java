867dfb45b982bf52d360061a4d5074736a06b307
Jusley Souza <jusleysouza31@gmail.com>
2022-12-11 19:30:05 -0300

    Criação e implementação do teste de setter

--- backend/CentralStoresCustomers/src/test/java/com/central/stores/customers/model/dto/RequestCustomerDTOTest.java
+++ backend/CentralStoresCustomers/src/test/java/com/central/stores/customers/model/dto/RequestCustomerDTOTest.java
@@ -23,6 +23,16 @@ class RequestCustomerDTOTest {
 		assertEquals(expectedREquestCustomerDTO.toString(), requestCustomerDTO.toString());
 	}
 	
-	
+	@Test
+	void setter() {
+		RequestCustomerDTO requestCostumerDTO = new RequestCustomerDTO();
+		requestCostumerDTO.setCpf("12365478965");
+		requestCostumerDTO.setEmail("caio@castro.com");
+		requestCostumerDTO.setGender("masculino");
+		requestCostumerDTO.setName("Caio Castro");
+		requestCostumerDTO.setPhone("1111111111");
+		requestCostumerDTO.setRg("325698741");
+		assertEquals(expectedREquestCustomerDTO.toString(), requestCostumerDTO.toString());
+	}
 
 }
