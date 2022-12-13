886f13b00965fad7214da1ceb2ffc5fa1d669768
Jusley Souza <jusleysouza31@gmail.com>
2022-12-12 23:45:51 -0300

    Implementando Serialização

--- backend/CentralStoresCustomers/src/main/java/com/central/stores/customers/model/Address.java
+++ backend/CentralStoresCustomers/src/main/java/com/central/stores/customers/model/Address.java
@@ -1,5 +1,6 @@
 package com.central.stores.customers.model;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.UUID;
 
@@ -27,8 +28,10 @@ import lombok.ToString;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Address {
+public class Address implements Serializable {
 
+	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(updatable = false, nullable = false )
