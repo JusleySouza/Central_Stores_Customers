e827551fb4bb67d2dcb6c4764b21b95e99d0a38c
Jusley Souza <jusleysouza31@gmail.com>
2023-01-02 17:46:04 -0300

    Mudança no objeto de retorno das rotas

--- backend/CentralStoresCustomers/src/main/java/com/central/stores/customers/controller/AddressController.java
+++ backend/CentralStoresCustomers/src/main/java/com/central/stores/customers/controller/AddressController.java
@@ -3,7 +3,6 @@ package com.central.stores.customers.controller;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,11 +11,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.central.stores.customers.model.Address;
 import com.central.stores.customers.model.dto.AddressDTO;
 import com.central.stores.customers.services.AddressServices;
-
-import lombok.Generated;
 
 @RestController
 @RequestMapping("/customers/{customerId}/address")
@@ -26,14 +22,14 @@ public class AddressController {
 	private AddressServices services;
 	
 	@PostMapping
-	public ResponseEntity<Address> create(@RequestBody AddressDTO requestAddressDTO, @PathVariable("customerId") UUID customerId) {
-		return new ResponseEntity<Address>(services.create(requestAddressDTO, customerId), HttpStatus.CREATED); 
+	public ResponseEntity<Object> create(@RequestBody AddressDTO requestAddressDTO, @PathVariable("customerId") UUID customerId) {
+		return services.create(requestAddressDTO, customerId); 
 	}
 	
 	@PutMapping("/{addressId}")
-	public ResponseEntity<Address> update(@RequestBody AddressDTO requestAddressDTO,  
+	public ResponseEntity<Object> update(@RequestBody AddressDTO requestAddressDTO,  
 			@PathVariable("addressId") UUID addressId){
-		return new ResponseEntity<Address>(services.update(requestAddressDTO, addressId), HttpStatus.OK);
+		return services.update(requestAddressDTO, addressId);
 	}
 
 }
