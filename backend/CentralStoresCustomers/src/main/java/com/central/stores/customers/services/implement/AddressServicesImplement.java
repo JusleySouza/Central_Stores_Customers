616facae7a3e784473b622ae93858421ea419903
Jusley Souza <jusleysouza31@gmail.com>
2023-01-02 17:53:01 -0300

    Adição da validação

--- backend/CentralStoresCustomers/src/main/java/com/central/stores/customers/services/implement/AddressServicesImplement.java
+++ backend/CentralStoresCustomers/src/main/java/com/central/stores/customers/services/implement/AddressServicesImplement.java
@@ -1,6 +1,10 @@
 package com.central.stores.customers.services.implement;
 
+import java.util.Set;
 import java.util.UUID;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,43 +17,64 @@ import com.central.stores.customers.mapper.UpdateModel;
 import com.central.stores.customers.model.Address;
 import com.central.stores.customers.model.Customer;
 import com.central.stores.customers.model.dto.AddressDTO;
+import com.central.stores.customers.model.dto.error.ResponseError;
 import com.central.stores.customers.repository.AddressRepository;
 import com.central.stores.customers.repository.CustomersRepository;
 import com.central.stores.customers.services.AddressServices;
 
 @Component
 public class AddressServicesImplement implements AddressServices {
-	
+
 	@Autowired
 	private CustomersRepository customerRepository;
-	
+
 	@Autowired
 	private AddressRepository addressRepository;
-	
+
 	@Autowired
 	private AddressMapper mapper;
 	
+	@Autowired
+	private Validator validator;
+
 	private Address address;
 	private Customer customer;
 
 	@Override
-	public Address create(AddressDTO requestAddressDTO, UUID customerId) {
+	public ResponseEntity<Object> create(AddressDTO requestAddressDTO, UUID customerId) {
+		
+		Set<ConstraintViolation<AddressDTO>> violations = validator.validate(requestAddressDTO);
+
+		if (!violations.isEmpty()) {
+			return new ResponseEntity<Object>(ResponseError.createFromValidations(violations),
+					HttpStatus.UNPROCESSABLE_ENTITY);
+		}
+		
 		address = mapper.toModel(requestAddressDTO);
 		addressRepository.save(address);
 		customer = customerRepository.findById(customerId).get();
 		customer.setAddress(address);
 		customerRepository.save(customer);
-		LoggerConfig.LOGGER_ADDRESS.info("Endereço do cliente " + customer.getName() + " salvo com sucesso!!");
-		return address;
+		LoggerConfig.LOGGER_ADDRESS.info("Customer address " + customer.getName() + " saved successfully!!");
+		return new ResponseEntity<Object>(address, HttpStatus.CREATED);
 	}
 
 	@Override
-	public Address update(AddressDTO requestAddressDTO, UUID addressId) {
+	public ResponseEntity<Object> update(AddressDTO requestAddressDTO, UUID addressId) {
+		
+		Set<ConstraintViolation<AddressDTO>> violations = validator.validate(requestAddressDTO);
+
+		if (!violations.isEmpty()) {
+			return new ResponseEntity<Object>(ResponseError.createFromValidations(violations),
+					HttpStatus.UNPROCESSABLE_ENTITY);
+		}
+		
 		address = addressRepository.findById(addressId).get();
 		address = UpdateModel.address(address, requestAddressDTO);
 		addressRepository.save(address);
-		LoggerConfig.LOGGER_ADDRESS.info("Endereço atualizado com sucesso!!");
-		return address;
+		LoggerConfig.LOGGER_ADDRESS.info("Address successfully updated!!");
+		return new ResponseEntity<Object>(address, HttpStatus.NO_CONTENT);
+	
 	}
 
 }
