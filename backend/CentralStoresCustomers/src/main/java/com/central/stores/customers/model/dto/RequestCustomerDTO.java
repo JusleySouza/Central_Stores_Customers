248358aec67ba0a2c6b35191b429a82cd5ac8378
Jusley Souza <jusleysouza31@gmail.com>
2023-01-02 17:48:26 -0300

    Adicionando as regras de validação

--- backend/CentralStoresCustomers/src/main/java/com/central/stores/customers/model/dto/RequestCustomerDTO.java
+++ backend/CentralStoresCustomers/src/main/java/com/central/stores/customers/model/dto/RequestCustomerDTO.java
@@ -1,5 +1,8 @@
 package com.central.stores.customers.model.dto;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,11 +18,18 @@ import lombok.ToString;
 @AllArgsConstructor
 public class RequestCustomerDTO {
 
+	@NotEmpty(message = "{name.not.empty}")
 	private String name;
+	@NotEmpty(message = "{cpf.not.empty}")
 	private String cpf;
+	@NotEmpty(message = "{rg.not.empty}")
 	private String rg;
+	@NotEmpty(message = "{gender.not.empty}")
 	private String gender;
+	@NotEmpty(message = "{phone.not.empty}")
 	private String phone;
+	@Email(message = "{email.not.valid}")
+	@NotEmpty(message = "{email.not.empty}")
 	private String email;
 
 }
