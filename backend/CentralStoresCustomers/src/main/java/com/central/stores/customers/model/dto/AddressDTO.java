6369c082fb183b73b5ccd8205e2876eece744628
Jusley Souza <jusleysouza31@gmail.com>
2023-01-02 17:47:35 -0300

    Adicionando as regras de validação

--- backend/CentralStoresCustomers/src/main/java/com/central/stores/customers/model/dto/AddressDTO.java
+++ backend/CentralStoresCustomers/src/main/java/com/central/stores/customers/model/dto/AddressDTO.java
@@ -1,5 +1,9 @@
 package com.central.stores.customers.model.dto;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,9 +19,14 @@ import lombok.ToString;
 @AllArgsConstructor
 public class AddressDTO {
 
+	@NotEmpty(message = "{street.not.empty}")
 	private String street;
+	@NotNull(message = "{number.not.null}")
+	@Min(value=1, message = "{number.not.less.than}")
 	private Integer number;
+	@NotEmpty(message = "{neighborhood.not.empty}")
 	private String neighborhood;
+	@NotEmpty(message = "{city.not.empty}")
 	private String city;
 
 }
