007e2f0eccaa19f5a280ec89f453fe5822b0feb1
Jusley Souza <jusleysouza31@gmail.com>
2023-01-02 02:07:23 -0300

    Implementando a interface Serializable

--- backend/CentralStoresCustomers/src/main/java/com/central/stores/customers/exception/ExceptionResponse.java
+++ backend/CentralStoresCustomers/src/main/java/com/central/stores/customers/exception/ExceptionResponse.java
@@ -1,13 +1,14 @@
 package com.central.stores.customers.exception;
 
+import java.io.Serializable;
 import java.util.Date;
 
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
-public class ExceptionResponse {
+public class ExceptionResponse implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
 	
 	private Date timestamp;
 	private String message;
