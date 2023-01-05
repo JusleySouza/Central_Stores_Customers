64d90e47654e1b0f14dd6c42d77215189d55e47e
Jusley Souza <jusleysouza31@gmail.com>
2023-01-05 01:43:35 -0300

    Remoção do teste de build

--- backend/CentralStoresCustomers/src/test/java/com/central/stores/customers/model/dto/AddressDTOTest.java
+++ backend/CentralStoresCustomers/src/test/java/com/central/stores/customers/model/dto/AddressDTOTest.java
@@ -17,13 +17,6 @@ class AddressDTOTest {
 	}
 
 	@Test
-	void builder() {
-		AddressDTO addressDTO = AddressDTO.builder().city("Bauru").neighborhood("Pedro Santos Drumond").number(56)
-				.street("Rua das Amelias").build();
-		assertEquals(expectedAddressDTO.toString(), addressDTO.toString());
-	}
-	
-	@Test
 	void setter() {
 		AddressDTO addressDTO = new AddressDTO();
 		addressDTO.setCity("Bauru");
