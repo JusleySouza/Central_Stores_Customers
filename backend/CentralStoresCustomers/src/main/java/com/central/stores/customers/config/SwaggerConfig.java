f699b53d3c5ed9e705c03f186bfbd3fa290fe67f
Jusley Souza <jusleysouza31@gmail.com>
2023-01-16 21:43:10 -0300

    Adicionando a anotação @Generated do Lombok

--- backend/CentralStoresCustomers/src/main/java/com/central/stores/customers/config/SwaggerConfig.java
+++ backend/CentralStoresCustomers/src/main/java/com/central/stores/customers/config/SwaggerConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import lombok.Generated;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -13,6 +15,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+@Generated
 public class SwaggerConfig extends WebMvcConfigurationSupport {
 
   @Bean
