a167adfa6e9a6df9c57e7e95f76b49d875c43391
Jusley Souza <jusleysouza31@gmail.com>
2023-01-18 01:15:27 -0300

    Retirada do mapstruct e criação do mapper

--- src/main/java/com/central/stores/customers/mapper/AddressMapper.java
+++ src/main/java/com/central/stores/customers/mapper/AddressMapper.java
@@ -1,20 +1,29 @@
 package com.central.stores.customers.mapper;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
+import java.time.LocalDate;
 
 import com.central.stores.customers.model.Address;
 import com.central.stores.customers.model.dto.AddressDTO;
 
-@Mapper(componentModel = "spring")
-public interface AddressMapper {
+public final class AddressMapper {
 	
-	@Mappings({
-		@Mapping(target= "created", expression = "java(java.time.LocalDate.now())"),
-		@Mapping(target= "id", ignore= true),
-		@Mapping(target= "changed", ignore= true),
-	})
-	Address toModel(AddressDTO addressDTO);
+	public static Address toModel(AddressDTO addressDTO) {
+		return Address.builder()
+				.street(addressDTO.getStreet())
+				.number(addressDTO.getNumber())
+				.neighborhood(addressDTO.getNeighborhood())
+				.city(addressDTO.getCity())
+				.created(LocalDate.now())
+				.build();
+	}
+	
+	public static Address updateAddress(Address address, AddressDTO RequestAddressDTO) {
+		address.setStreet(RequestAddressDTO.getStreet());
+		address.setNumber(RequestAddressDTO.getNumber());
+		address.setNeighborhood(RequestAddressDTO.getNeighborhood());
+		address.setCity(RequestAddressDTO.getCity());
+		address.setChanged(LocalDate.now());
+		return address;
+	}
 
 }
